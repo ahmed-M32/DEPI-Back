3d4180786215136d5c603a871922a4345da5fbf3
ahmed-M32 <am.3443rrrr@gmail.com>
2025-05-07 21:05:31 +0300

    fix: Update error message for missing JWT token in authentication middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,10 +5,11 @@ export const checkAuthentication = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
 
+
         if (!token) {
             return res
                 .status(401)
-                .json({ message: "Unauthorized- token doesn't exist" });
+                .json({ message: req.cookies });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
